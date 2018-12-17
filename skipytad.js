eedff042cb77d8b3f739c06c8be6e1f7a71e27ef
Alexander Frize <alexander.frize@gmail.com>
2018-12-16 18:44:58 -0800

    Version 1.1

--- skipytad.js
+++ skipytad.js
@@ -1,19 +1,22 @@
-(function () {
+(function() {
   function skipYTAd() {
     /* Skippes big video */
-    var bigAdSkipButton_arr = document.getElementsByClassName("videoAdUiSkipButtonExperimentalText videoAdUiFixedPaddingSkipButtonText");
+    var bigAdSkipButton_arr = document.getElementsByClassName(
+      "ytp-ad-skip-button ytp-button"
+    );
     if (bigAdSkipButton_arr.length !== 0) {
       var bigAdSkipButton = bigAdSkipButton_arr[0];
       bigAdSkipButton.click();
-      console.log("skipYTAd()::bigAdSkipButton.click()")
+      console.log("skipYTAd()::bigAdSkipButton.click()");
     }
 
-    var smallAdCloseButton_container = document.getElementsByClassName("close-padding condensed contains-svg");
+    var smallAdCloseButton_container = document.getElementsByClassName(
+      "ytp-ad-overlay-close-button"
+    );
     if (smallAdCloseButton_container.length !== 0) {
       smallAdCloseButton_container[0].click();
       console.log("skipYTAd()::smallAdCloseButton_container[0].click()");
     }
-
   }
   setInterval(skipYTAd, 2000);
   console.log("skipYTAd extention is running...");
